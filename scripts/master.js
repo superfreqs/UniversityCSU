930c63a60ea3bedf6ba2f2ea2a06eb3b522d8091
bap2578 <bap2578@gmail.com>
2018-01-23 11:59:57 -0500

    FIXED THE YEARTERM FINALLY

--- scripts/master.js
+++ scripts/master.js
@@ -7,15 +7,18 @@ $(document).ready(function(){
     // get value of floor
     var floor = $('#floor').val();
 
-    // get value of year
+    // get value of year   maybe use Number()
     var year = $('#year').val();
 
     // get value of Semester
     var semester = $('#semester').val();
 
+    // get value of yearterm MAYBE
+    var yearterm = eval(Number(year) + Number(semester));
+
     // example url DO_Floor_ 1_200610.html
 
-    var floorplanURL = building + '_Floor_' + floor + year + semester + '.html';
+    var floorplanURL = building + '_Floor_' + floor + yearterm + '.html';
 
     console.log(floorplanURL, 'building: ', building, 'floor', floor, 'semester: ', semester , 'year: ', year);
 
